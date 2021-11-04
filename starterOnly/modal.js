4e3c646aaa312f67aeb9e90903171426ca4e0ad9
Margaux Leroy <margauxl-1994@hotmail.fr>
2021-11-04 21:24:01 +0100

    feat: fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Fermer la modale #1
+closeBtn.addEventListener("click", closeModal);
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Implémenter entrées du formulaire #2
