62d8b9fdea7785bf4aa6d9093385a9f87b885944
Margaux Leroy <margauxl-1994@hotmail.fr>
2021-11-19 14:58:47 +0100

    feat: #2 Conditions de validation du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -51,6 +51,74 @@ closeBtn.addEventListener("click", closeModal);
 ///// #2 IMPLÉMENTER ENTRÉES DU FORMULAIRE ////////
 ////////////////////////////////////////////////////
 
+function validate() {
+  // Créé une "copie" du formulaire qui donne accès aux valeurs des champs
+  const form = document.querySelector("form");
+  console.log(form);
+  const data = new FormData(form);
+
+  const first = data.get("first");
+  const last = data.get("last");
+  const email = data.get("email");
+  const birthdate = data.get("birthdate");
+  const quantity = data.get("quantity");
+  const location = data.get("location");
+
+  // Conditions pour que le formulaire soit valide
+  if (first.length < 2) {
+    displayErrorFeedback(
+      firstInput,
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+    return false;
+  }
+
+  if (last.length < 2) {
+    displayErrorFeedback(
+      lastInput,
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    );
+    return false;
+  }
+
+  if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    displayErrorFeedback(
+      emailInput,
+      "Veuillez saisir une adresse email valide."
+    );
+    return false;
+  }
+
+  const dateTime = new Date(birthdate).getTime();
+  if (isNaN(dateTime)) {
+    displayErrorFeedback(
+      birthdateInput,
+      "Vous devez entrer votre date de naissance."
+    );
+    return false;
+  }
+
+  if (isNaN(new Number(quantity)) || quantity === "") {
+    displayErrorFeedback(quantityInput, "Vous devez entrer un nombre.");
+    return false;
+  }
+
+  if (location === null) {
+    displayErrorFeedback(cityOptions, "Vous devez choisir une option.");
+    return false;
+  }
+
+  if (!checkboxConditions.checked) {
+    displayErrorFeedback(
+      checkboxWarning,
+      "Vous devez vérifier que vous acceptez les termes et conditions."
+    );
+    return false;
+  }
+
+  return true;
+}
+
 form.onsubmit = (event) => {
   const isValid = validate();
   if (isValid === false) {
